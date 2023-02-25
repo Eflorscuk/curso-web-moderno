5b3ec7ab3c2fc169cece049e7646c5386f699412
Evandro Florscuk <evandro_ff@me.com>
2023-02-24 23:25:39 -0300

    function

---
+++ function/paramPadrao.js
@@ -0,0 +1,20 @@
+// estrategia 1 para gerar valor padrão
+function soma1(a, b, c){
+    a = a || 1
+    b = b || 1
+    c = c || 1
+    return a + b + c
+}
+
+// Estratégias 2, 3 e 4 para gerar valor padrão
+function soma2(a, b, c){
+    a = a !== undefined ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+    return a + b + c
+}
+
+function soma3(a = 1, b = 2, c = 3){
+    return a + b + c
+}
+
